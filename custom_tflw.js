bc7e9ca33244db0918eb1c3a278749d4bbf67255
Lars Bergmann <lars@bergmann82.de>
2023-01-24 22:27:19 +0100

    Implement caleidoscope sektch in tfjs demo

--- custom_tflw.js
+++ custom_tflw.js
@@ -1,49 +1,48 @@
 $(function() {
     let sketch = function(p) {
 
-        p.setup = function() {
-            p.createCanvas(640, 480);
-            p.background(0);
-            p.noStroke();
-        }
+        let symmetry = 6;   
+
+        let angle = 360 / symmetry;
         
-        p.draw = function() {
-            // Create an alpha blended background
-            p.fill(0, 10);
-            p.rect(0, 0, p.width, p.height);
-
-            if (playing) {
-
-                let new_n_x = GLOBAL_POSES[0].keypoints[1].x
-                let new_n_y = GLOBAL_POSES[0].keypoints[1].y
-                let new_n_z = GLOBAL_POSES[0].keypoints[1].score
-
-                console.log(new_n_x, new_n_y, new_n_z)
-
-                p.fill(200);
-
-                p.ellipse(
-                    new_n_x,
-                    new_n_y,
-                    64 + 64 * new_n_z,
-                    64 + 64 * new_n_z
-                );
-
-                let new_n_x2 = GLOBAL_POSES[0].keypoints[2].x
-                let new_n_y2 = GLOBAL_POSES[0].keypoints[2].y
-                let new_n_z2 = GLOBAL_POSES[0].keypoints[2].score
-                
-                p.fill(200);
-
-                p.ellipse(
-                    new_n_x2,
-                    new_n_y2,
-                    64 + 64 * new_n_z2,
-                    64 + 64 * new_n_z2
-                );
-
-            }
+        p.setup = function() { 
+          p.createCanvas(710, 710);
+          p.angleMode(p.DEGREES);
+          p.background(127);
         }
+                
+        p.draw = function() {
+
+          if (!GLOBAL_POSES) {
+            return;
+          }
+          p.translate(p.width / 2, p.height / 2);
+
+          let new_n_x = GLOBAL_POSES[0].keypoints[1].x
+          let new_n_y = GLOBAL_POSES[0].keypoints[1].y
+
+          let new_n_x2 = GLOBAL_POSES[0].keypoints[2].x
+          let new_n_y2 = GLOBAL_POSES[0].keypoints[2].y
+
+
+          if (new_n_x > 0 && new_n_x < p.width && new_n_y > 0 && new_n_y < p.height) {
+            let mx = new_n_x - p.width / 2;
+            let my = new_n_y - p.height / 2;
+            let pmx = new_n_x2 - p.width / 2;
+            let pmy = new_n_y2 - p.height / 2;
+            
+            for (let i = 0; i < symmetry; i++) {
+                p.rotate(angle);
+                p.strokeWeight(1);
+                p.line(mx, my, pmx, pmy);
+                p.push();
+                p.scale(1, -1);
+                p.line(mx, my, pmx, pmy);
+                p.pop();
+            }
+          }
+        }
+
     };
     new p5(sketch, 'p5_stage');
 })
